7b16f746edb8f22dc45aafc110defcf6293ac4b7
Eliran <EliranShmila123@gmail.com>
2018-06-26 18:11:45 +0300

    changed homography computation method

--- image_stitching.py
+++ image_stitching.py
@@ -1,16 +1,16 @@
 # import the necessary packages
-import scipy.signal
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-import scipy.signal
-
+from cv2.cv2 import threshold
+from scipy import linalg
+import sys
 
 '''
 In this project you are asked to implement image stitching procedure
 - Find out detectors and their description
 - Match the detections between two images
-- Compute homography and filters the outliers 
+- Compute homography and filters the outliers
 - Apply projection to stitch the image s
 '''
 
@@ -19,6 +19,56 @@ class Stitcher:
     def __init__(self):
         # determine if we are using OpenCV v3.X
         self.isv3 = (int(cv2.__version__[0]) == 3)
+
+    def findHomographyMatrix(self, x1, y1, x2, y2):
+        A = np.array([[]])
+        for i in range(4):
+            a = np.array([[-x1[i], -y1[i], -1, 0, 0, 0, x2[i] * x1[i], x2[i] * y1[i], x2[i]]])
+            b = np.array([[0, 0, 0, -x1[i], -y1[i], -1, y2[i] * x1[i], y2[i] * y1[i], y2[i]]])
+            temp = np.concatenate((a, b))
+            if i == 0:
+                A = np.copy(temp)
+            else:
+                A = np.concatenate((A, temp))
+        l, v, r = np.linalg.svd(A)
+        # we want to get the eigen-vector that corresponds to the smallest eigen-value (closest to zero)
+        return r[-1]
+
+    def findHomography(self, ptsA, ptsB, threshold, maxIter=500):
+        def reprojError(x, y):
+            return \
+                np.linalg.norm(np.dot(h, np.append(x, 1)) - np.append(y, 1)) + \
+                np.linalg.norm(np.dot(h_inv, np.append(y, 1)) - np.append(x, 1))
+
+        N = len(ptsA)
+        indices = range(N)
+        status = [0 for i in range(len(ptsA))]
+        # np.random.shuffle(indices)
+        index = 0
+        best_h = np.random.random((3, 3))
+        h = best_h
+        h_inv = linalg.pinv(h)
+        min_error = sum(reprojError(ptsA[i], ptsB[i]) for i in range(N)) / (2 * N)
+
+        for j in range(maxIter):
+            arr = np.random.randint(0, N - 1, 4)
+            xy1 = ptsA[arr]
+            xy2 = ptsB[arr]
+
+            # h, _= cv2.findHomography(ptsA[arr], ptsB[arr], cv2.RANSAC, threshold)
+            h = self.findHomographyMatrix(xy1[:, 0], xy1[:, 1], xy2[:, 0], xy2[:, 1])
+            h = np.reshape(h, (3, 3))
+            h_inv = linalg.pinv(h)
+            reprojection_error = sum(reprojError(ptsA[i], ptsB[i]) for i in range(N)) / N
+            # print reprojection_error
+
+            if reprojection_error < min_error:
+                best_h, min_error = h, reprojection_error
+                # threshold = (max(reprojError(ptsA[i], ptsB[i]) for i in range(N))+min(reprojError(ptsA[i], ptsB[i]) for i in range(N)))/2
+
+        status = [reprojError(ptsA[i], ptsB[i]) < min_error * 2 for i in range(N)]
+
+        return (best_h, status)
 
     def stitch(self, images, ratio=0.75, reprojThresh=4.0,
                showMatches=False):
@@ -41,9 +91,8 @@ class Stitcher:
         # otherwise, apply a perspective warp to stitch the images
         # together
         (matches, H, status) = M
-        # result = cv2.warpPerspective(imageA, H,
-        #                              (imageA.shape[1] + imageB.shape[1], imageA.shape[0]))
-        result = self.my_warp_perspective(imageA,H,(imageA.shape[1] + imageB.shape[1], imageA.shape[0]))
+        result = cv2.warpPerspective(imageA, H,
+                                     (imageA.shape[1] + imageB.shape[1], imageA.shape[0]))
         result[0:imageB.shape[0], 0:imageB.shape[1]] = imageB
 
         # check to see if the keypoint matches should be visualized
@@ -58,103 +107,59 @@ class Stitcher:
         # return the stitched image
         return result
 
-    def my_warp_perspective(self,imageA, H,dest_shape):
-        width,height = dest_shape
-        res = np.zeros((height,width,3)).astype(imageA.dtype)
-        a_rows,a_cols,_ = imageA.shape
-        index_mat = np.zeros((a_rows,a_cols,2))
-
-        x,y = np.meshgrid(range(a_cols),range(a_rows))
-        o = np.ones(x.shape)
-        src_vecs = np.array([np.reshape(x,[-1]),np.reshape(y,[-1]),np.reshape(o,[-1])])
-        mul_sum = np.dot(H,src_vecs)
-        dst_x = mul_sum[0,:] / mul_sum[2,:]
-        dst_y = mul_sum[1,:] / mul_sum[2,:]
-        dst_x = dst_x.reshape(a_rows,a_cols)
-        dst_y = dst_y.reshape(a_rows,a_cols)
-
-        dst_x = dst_x.astype(np.int)
-        dst_y = dst_y.astype(np.int)
-        ind = np.where((dst_x>=0) * (dst_y>=0))
-        res[dst_y[ind],dst_x[ind]]=imageA[y[ind],x[ind]]
-        # do conv with kernal of size 5
-        # kernel = np.array([0.05,0.25,0.4,0.25,0.05])
-        # res[:,:,0] = scipy.signal.convolve2d(res[:,:,0].reshape(height,width), kernel, 'same')
-        # res[:,:,1] = scipy.signal.convolve2d(res[:,:,1].reshape(height,width), kernel, 'same')
-        # res[:,:,2] = scipy.signal.convolve2d(res[:,:,2].reshape(height,width), kernel, 'same')
-
-        # cv2.imshow('check', res)
-        # cv2.waitKey()
-        return res
-
-    # def rgb_bilinear_interpolation(self,image):
-    #     rows,cols = image.shape
+    # def detectAndDescribe(self, image):
+    #     # convert the image to grayscale
+    #     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     #
-    # def rgb_b_inter_helper(self,x,y,points):
-    #     """
+    #     # check to see if we are using OpenCV 3.X
+    #     if self.isv3:
+    #         # detect and extract features from the image
+    #         descriptor = cv2.xfeatures2d.SIFT_create()
+    #         (kps, features) = descriptor.detectAndCompute(image, None)
     #
-    #     :param x: the x value of the point to interpolate
-    #     :param y: the y value of the point
-    #     :param points: the neighbors points as array that each cell is list of [xi,yi,[r,g,b]]
-    #     :return: the r,g,b value s of the point
-    #     """
-    #     s_points = sorted(points) # order points by x, then by y
-
-    @staticmethod
-    def shifted9(mat):
-        all_mat = [mat.copy() for _ in range(9)]
-        all_mat[0][1:, 1:] = mat[:-1, :-1]
-        all_mat[1][:, 1:] = mat[:, :-1]
-        all_mat[2][:-1, 1:] = mat[1:, :-1]
-        all_mat[3][1:, :] = mat[:-1, :]
-        all_mat[4][:, :] = mat[:, :]
-        all_mat[5][:-1, :] = mat[1:, :]
-        all_mat[6][1:, :-1] = mat[:-1, 1:]
-        all_mat[7][:, :-1] = mat[:, 1:]
-        all_mat[8][:-1, :-1] = mat[1:, 1:]
-        return all_mat
-
-    @classmethod
-    def sum9(cls, mat):
-        return np.sum(cls.shifted9(mat), axis=0)
+    #     # otherwise, we are using OpenCV 2.4.X
+    #     else:
+    #         # detect keypoints in the image
+    #         detector = cv2.FeatureDetector_create("SIFT")
+    #         kps = detector.detect(gray)
+    #
+    #         # extract features from the image
+    #         extractor = cv2.DescriptorExtractor_create("SIFT")
+    #         (kps, features) = extractor.compute(gray, kps)
+    #
+    #     # convert the keypoints from KeyPoint objects to NumPy
+    #     # arrays
+    #     kps = np.float32([kp.pt for kp in kps])
+    #
+    #     # return a tuple of keypoints and features
+    #     return (kps, features)
 
     def detectAndDescribe(self, image):
         # convert the image to grayscale
-        keypoints, descriptors = self.describe(image, self.detect(image))
-        keypoints = np.float32([kp.pt for kp in keypoints])
-        return keypoints, descriptors
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    def detect(self, image):
-        """
-        Harris Corner Detector
-        :param image: RGB Matrix
-        :return: list of cv2.KeyPoint
-        """
-        K = 0.05
-        gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY).astype(np.float) / 255.
-        x_kernel = np.array([[-1, 0, 1]] * 3)
-        Ix = scipy.signal.convolve2d(gray, x_kernel, 'same')
-        Iy = scipy.signal.convolve2d(gray, x_kernel.T, 'same')
+        # check to see if we are using OpenCV 3.X
+        if self.isv3:
+            # detect and extract features from the image
+            descriptor = cv2.xfeatures2d.SIFT_create()
+            (kps, features) = descriptor.detectAndCompute(image, None)
 
-        MIxx = self.sum9(Ix ** 2)
-        MIyy = self.sum9(Iy ** 2)
-        MIxy = self.sum9(Ix * Iy)
+        # otherwise, we are using OpenCV 2.4.X
+        else:
+            # detect keypoints in the image
+            detector = cv2.FeatureDetector_create("SIFT")
+            kps = detector.detect(gray)
 
-        img_det = (MIxx * MIyy) - (MIxy * MIxy)
-        img_trace = MIxx + MIyy
+            # extract features from the image
+            extractor = cv2.DescriptorExtractor_create("SIFT")
+            (kps, features) = extractor.compute(gray, kps)
 
-        img_corners = (img_det - K * img_trace ** 2) > 0
-        np_keypoints = np.argwhere(img_corners)
-        np_keypoints = [np_keypoints[i] for i in xrange(0, len(np_keypoints), 20)]
-        keypoints = [cv2.KeyPoint(x, y, 30) for x, y in np_keypoints]
-        return keypoints
+        # convert the keypoints from KeyPoint objects to NumPy
+        # arrays
+        kps = np.float32([kp.pt for kp in kps])
 
-    def describe(self, image, keypoints):
-        if not self.isv3:
-            raise RuntimeError("OpenCV version should be 3.X")
-        sift = cv2.xfeatures2d.SIFT_create()
-        matched_keypoints, descriptors = sift.compute(image, keypoints)
-        return matched_keypoints, descriptors
+        # return a tuple of keypoints and features
+        return (kps, features)
 
     def matchKeypoints(self, featuresA, featuresB):
         # compute the raw matches and initialize the list of actual
@@ -173,7 +178,10 @@ class Stitcher:
         return matches
 
     def computeHomography(self, kpsA, kpsB, matches, reprojThresh):
-
+        # print kpsA
+        # print kpsB
+        # print matches
+        # print reprojThresh
         # computing a homography requires at least 4 matches
         if len(matches) > 4:
             # construct the two sets of points
@@ -181,8 +189,10 @@ class Stitcher:
             ptsB = np.float32([kpsB[i] for (i, _) in matches])
 
             # compute the homography between the two sets of points
-            (H, status) = cv2.findHomography(ptsA, ptsB, cv2.RANSAC,
-                                             reprojThresh)
+
+            # (H, status) = cv2.findHomography(ptsA, ptsB, cv2.RANSAC, reprojThresh)
+            # ptsA, ptsB are lists of the same size. index-correspondness
+            (H, status) = self.findHomography(ptsA, ptsB, reprojThresh)
 
             # return the matches along with the homograpy matrix
             # and status of each matched point
